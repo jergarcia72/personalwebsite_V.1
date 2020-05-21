3b128f431a8342055910e578a1123b234c844e51
Jeremy <contact@jergarcia.com>
2020-05-21 11:02:07 -0600

    thom helped me with everything

--- js/gallery.js
+++ js/gallery.js
@@ -1,41 +1,63 @@
-const gallerySlide = document.querySelector('.gallery-slide');
-const galleryImages = document.querySelectorAll('.gallery-slide img');
+const galleryContainers = document.querySelectorAll('.gallery-container');
+const magicPad = 3.5;
 
-//nav
-const nextTag = document.querySelector(".next");
-const prevTag = document.querySelector(".prev");
+function sizeGallery(){
+    galleryContainers.forEach(galleryContainer => {
+        let counter = 1;
+        const gallerySlide = galleryContainer.querySelector('.gallery-slide');
+        const galleryImages = galleryContainer.querySelectorAll('.gallery-image-container');
+                //nav
+        const nextTag = galleryContainer.querySelector(".next");
+        const prevTag = galleryContainer.querySelector(".prev");
+        const size = galleryContainer.clientWidth;
+        gallerySlide.style.width =  ((galleryContainer.clientWidth * galleryImages.length) + magicPad) + "px";
+        
+        galleryImages.forEach(image => {
+            image.style.width=(size - magicPad)+'px';
+        });
+        
+        gallerySlide.style.transform = 'translateX(' + (-size * counter )+ 'px)';
+        
+        //nav listeners
+        nextTag.addEventListener('click', () => {
+            if (counter >= galleryImages.length - 1) return;
+            gallerySlide.style.transition = "transform 0.4s ease-in-out";
+            counter++;
+            gallerySlide.style.transform = 'translateX(' + (-size * counter )+ 'px)';
+        });
+        
+        prevTag.addEventListener('click', ()=>{
+            if(counter <= 0) return;
+            gallerySlide.style.transition = "transform 0.4s ease-in-out";
+            counter--;
+            gallerySlide.style.transform = 'translateX(' + (-size * counter )+ 'px)';
+        });
+        
+        gallerySlide.addEventListener('transitionend', (event)=>{
+            if (galleryImages[counter].id === 'lastClone'){
+                gallerySlide.style.transition = "none";
+                counter = galleryImages.length - 2;
+                gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+            }
+            if (galleryImages[counter].id === 'firstClone'){
+                gallerySlide.style.transition = "none";
+                counter = galleryImages.length - counter;
+                gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+            }
+        });
+    })
+}
 
-//counter
-let counter = 1;
-const size = galleryImages[0].clientWidth;
+sizeGallery();
 
-gallerySlide.style.transform = 'translateX(' + (-size * counter )+ 'px)';
+function debounce(func){
+    var timer;
+    return function(event){
+      if(timer) clearTimeout(timer);
+      timer = setTimeout(func,100,event);
+    };
+  }
 
-//nav listeners
-
-nextTag.addEventListener('click', () => {
-    if (counter >= galleryImages.length - 1) return;
-    gallerySlide.style.transition = "transform 0.4s ease-in-out";
-    counter++;
-    gallerySlide.style.transform = 'translateX(' + (-size * counter )+ 'px)';
-});
-
-prevTag.addEventListener('click', ()=>{
-    if(counter <= 0) return;
-    gallerySlide.style.transition = "transform 0.4s ease-in-out";
-    counter--;
-    gallerySlide.style.transform = 'translateX(' + (-size * counter )+ 'px)';
-});
-
-gallerySlide.addEventListener('transitionend', ()=>{
-    if (galleryImages[counter].id === 'lastClone'){
-        gallerySlide.style.transition = "none";
-        counter = galleryImages.length - 2;
-        gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-    }
-    if (galleryImages[counter].id === 'firstClone'){
-        gallerySlide.style.transition = "none";
-        counter = galleryImages.length - counter;
-        gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-    }
-});
+window.addEventListener("resize",debounce(function(e){
+    sizeGallery();
+}));
