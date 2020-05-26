911bacca136116126e5f908275fb2643bae61204
Jeremy <contact@jergarcia.com>
2020-05-25 20:38:41 -0600

    add motion page

--- js/linkhover.js
+++ js/linkhover.js
@@ -1,9 +1,11 @@
 var instaVidHtml = document.querySelector("#insta-video");
 var emailVidHtml = document.querySelector("#email-video");
+var motionVidHtml = document.querySelector("#motion-video");
 // var videoAssets = ['assets/home/insta.mp4', 'assets/home/email.mp4'];
 
 var instaHTML = document.querySelector('.insta');
 var emailHTML = document.querySelector('.email');
+var motionHTML = document.querySelector('.motion');
 
 instaHTML.addEventListener("mouseenter", function (event) {
   instaVidHtml.style.opacity = 1;
@@ -19,5 +21,13 @@ emailHTML.addEventListener("mouseenter", function (event) {
 
 emailHTML.addEventListener("mouseout", function (event) {
   emailVidHtml.style.opacity = 0;
+});
+
+motionHTML.addEventListener("mouseenter", function (event) {
+  motionVidHtml.style.opacity = 1;
+});
+
+motionHTML.addEventListener("mouseout", function (event) {
+  motionVidHtml.style.opacity = 0;
 });
 
