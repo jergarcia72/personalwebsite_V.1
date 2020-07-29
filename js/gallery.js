010190b430c6ffecedf78d65da7308da647a302a
Jeremy Garcia <contact@jergarcia.com>
2020-07-28 21:53:45 -0600

    description change

--- js/gallery.js
+++ js/gallery.js
@@ -50,6 +50,7 @@ function sizeGallery(){
 
 sizeGallery();
 
+
 function debounce(func){
     var timer;
     return function(event){
@@ -61,3 +62,21 @@ function debounce(func){
 window.addEventListener("resize",debounce(function(e){
     sizeGallery();
 }));
+
+
+// ACCORDIAN JAVA
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
